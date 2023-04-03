fbc28d2f71a0fcaf8507638a6058d889a1b53cf6
Canfieldr <canfieldr@merrimack.edu>
2023-04-02 20:36:32 -0400

    Finished preprocessing
    moved logregsgd file into the src
    finished and changed some of the preprocessing commands to start working on building the models made sure to keep cloud columns by replacing na it -1
    changed for loop to and used a pandas command to get the mean finished getting all the averages for the rest of the columns. Made sure that the data types are the same. added some spaces to make code easier to read for me

--- src/Model.py
+++ src/Model.py
@@ -1,8 +1,15 @@
 # ooking into locaion variable and changing poosibly chaning it making a variable for location
 
 import pandas as pd # Used for data analysis and manipulation.
+import numpy as np # Used for arrays, matrices, and high-level math functions. 
+import matplotlib.pyplot as plt # Used for graphics and visuals.
+import os # Helps with operating system
 
-# Set Pandas options to display more columns
+from sklearn.model_selection import train_test_split # Used for splitting data
+from sklearn.preprocessing import MinMaxScaler # Used for scaling data
+from sklearn.preprocessing import StandardScaler # Used also for scaling data 
+
+"""# Set Pandas options to display more columns
 pd.options.display.max_columns=50
 
 # Read in the weather data csv
@@ -11,3 +18,6 @@ df=pd.read_csv('weatherAUS.csv', encoding='utf-8')
 Location_list = df['Location'].tolist()
 
 print(Location_list)
+
+        """
+
