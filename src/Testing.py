5f9b57521b95052ce90bd37f0f56b4cf7e0f18f2
williamhancock328 <williamhancock328@gmail.com>
2023-04-11 20:03:11 -0400

    gui started

--- src/Testing.py
+++ src/Testing.py
@@ -2,6 +2,7 @@ import pandas as pd # Used for data analysis and manipulation.
 import numpy as np # Used for arrays, matrices, and high-level math functions. 
 import matplotlib.pyplot as plt # Used for graphics and visuals.
 import os # Helps with operating system
+import tkinter as tk # Used for GUI
 
 from Preprocessor import preproccess
 from sklearn.model_selection import train_test_split # Used for splitting data
@@ -11,6 +12,7 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.svm import LinearSVR
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, accuracy_score
+
 
 from Model import SVRModel, LogRegModel, MLPModel
 
@@ -36,3 +38,21 @@ MLPcmd = ConfusionMatrixDisplay(MLPcf_matrix)
 MLPcmd.plot(cmap = 'ocean')
 MLPcmd.ax_.set(xlabel = 'Predicted', ylabel = 'True', title = 'MLP will it rain tomorrow')
 plt.show()
+
+def initGUI(g):
+    windoe = tk.Tk()
+    length = 1200
+    width = 700
+    dimension = str(length) + 'x' + str(width)
+    # Creating window color is not white so we can see the images
+    # Creating a Frame to view the images
+    windoe.geometry(dimension)  
+    windoe.title('3335_Project4')
+    windoe.configure(background = 'white')
+
+    gobutton = tk.Button(windoe, text = 'Go', width = 10, font = my_font1, command = lambda: button_click(stepcounter, g, labels, windoe))
+
+    gobutton.grid(row = 7, column = 6, padx = 20, pady = 35, sticky = (N, S, E, W))
+
+
+    windoe.mainloop() 
